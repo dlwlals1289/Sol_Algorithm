1d24ef2fd18f2ce7d81f940b6d058d7cce19ea81
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2023-08-02 16:17:06 +0900

    [Gold V] Title: LCS, Time: 584 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,16 @@
+# LCS : 최장 공통 부분 수열
+import sys
+
+str1 = input()
+str2 = input()
+m = len(str1)
+n = len(str2)
+dp = [[0]*(n+1) for _ in range(m+1)]
+
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        if str1[i-1] == str2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+print(dp[-1][-1])
