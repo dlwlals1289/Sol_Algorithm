00690182df3e65179794a42275e0c11acc14fcc8
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2024-01-21 14:56:20 +0900

    [Silver III] Title: N과 M (3), Time: 1388 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,20 @@
+# N과 M(3)
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+answer = []
+
+def dfs():
+    if len(answer) == M:
+        print(' '.join(map(str, answer)))
+        return
+    
+    for i in range(1, N+1):
+        
+        answer.append(i)
+        dfs()
+        answer.pop()
+
+dfs()
