1af90925e35cec219203d1361dd29da7242c39b8
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2024-02-25 17:18:27 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -3,13 +3,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-cost = []
-for i in range(n):
-    cost.append(list(map(int, input().split())))
+price = []
 
-for i in range(1,n):
-    cost[i][0] = min(cost[i-1][1], cost[i-1][2]) + cost[i][0]
-    cost[i][1] = min(cost[i-1][0], cost[i-1][2]) + cost[i][1]
-    cost[i][2] = min(cost[i-1][0], cost[i-1][1]) + cost[i][2]
+for _ in range(n):
+    price.append(list(map(int, input().split())))
 
-print(min(cost[n-1]))
+
+for i in range(1, n):
+    price[i][0] = min(price[i-1][1], price[i-1][2]) + price[i][0]
+    price[i][1] = min(price[i-1][0], price[i-1][2]) + price[i][1]
+    price[i][2] = min(price[i-1][0], price[i-1][1]) + price[i][2]
+
+print(min(price[n-1]))
