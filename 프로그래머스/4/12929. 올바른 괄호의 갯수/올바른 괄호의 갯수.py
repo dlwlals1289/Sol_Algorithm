08371c8841ee9bb1ef7eaca04d573f49362ab3ce
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2024-05-17 13:43:52 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -0,0 +1,9 @@
+def solution(n):
+    
+    dp = [0]*(n+1)
+    dp[0], dp[1] = 1, 1
+    
+    for i in range(2, n+1):
+        for j in range(i):
+            dp[i] += dp[j] * dp[i-j-1]
+    return dp[n]
