e1b4c340483718ecc17c864da4100dc03dde4547
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2024-10-10 13:02:06 +0900

    [level 3] Title: 네트워크, Time: 1.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,24 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(100000)
+            
 def solution(n, computers):
     answer = 0
-    
-    network = [[] for _ in range(n)]
+    networks = [[] for _ in range(n)]
     visited = []
+    
+    def dfs(a):
+        for i in networks[a]:
+            if i not in visited:
+                visited.append(i)
+                dfs(i)
     
     for i in range(n):
         for j in range(n):
-            if i != j and computers[i][j] == 1:
-                network[i].append(j)
+            if computers[i][j] == 1 and i != j:
+                networks[i].append(j)
                 
-    def dfs(a):
-        for node in network[a]:
-            if node not in visited:
-                visited.append(node)
-                dfs(node)
-    
     for i in range(n):
         if i not in visited:
             visited.append(i)
-            answer += 1
             dfs(i)
-    
+            answer += 1
+
     return answer
