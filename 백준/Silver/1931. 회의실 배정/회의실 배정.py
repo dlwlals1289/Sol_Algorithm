dbd6a71866b256681498109f97c4467ceb4ff95e
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2024-10-25 12:24:02 +0900

    [Silver I] Title: 회의실 배정, Time: 256 ms, Memory: 58284 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,18 +1,16 @@
+# 회의실배정 
 import sys
+input = sys.stdin.readline
 
 n = int(input())
-schedule = []
-answer = [[0,0]]
+time = [list(map(int, input().split())) for _ in range(n)]
+time.sort(key= lambda x : (x[1], x[0]))
+answer = 1
 
-for i in range(n):
-    schedule.append(list(map(int, input().split())))
+end = time[0][1]
+for i in range(1, n):
+    if end <= time[i][0]:
+        answer += 1
+        end = time[i][1]
 
-schedule.sort(key = lambda x:(x[1] ,x[0])) # 끝나는 시간 기준 오름차순 정렬 한 후 시작하는 시간도 오름차순 정렬
-
-for i in schedule:
-    start = answer[-1][0]
-    end = answer[-1][1]
-    
-    if end <= i[0]:
-        answer.append(i)
-print(len(answer)-1)
+print(answer)
