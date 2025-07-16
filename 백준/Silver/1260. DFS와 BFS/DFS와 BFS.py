33c17f81484074845e812f8a35881c80ed4c3656
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-07-16 17:32:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 80 ms, Memory: 35684 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,41 +1,52 @@
 # DFS와 BFS
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def dfs_recursive(graph, start, visited = []): # DFS
-
-    visited.append(start)
- 
-    for node in graph[start]:
-        if node not in visited:
-            dfs_recursive(graph, node, visited)
-    return visited
-
-def bfs(graph, start_node): # BFS
-    need_visited, visited = [], []
-    need_visited.append(start_node)
-
-    while need_visited:
-        node = need_visited[0]
-        del need_visited[0]
-
-        if node not in visited:
-            visited.append(node)
-            need_visited.extend(graph[node])
-            # print(need_visited)
-    return visited
-
 n,m,v = map(int, input().split())
-
-graph = [[]*n for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+arr = [[] for _ in range(n+1)]
+result = []
+result2 = []
 
 for i in range(m):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    arr[a].append(b)
+    arr[b].append(a)
 
-for i in range(1, n+1):
-    graph[i].sort()
+for i in arr:
+    i.sort()
+    
 
-print(*dfs_recursive(graph, v))
-print(*bfs(graph, v))
+def dfs(x) :
+    visited[x] = True
+    result.append(x)
+    for i in arr[x]:
+        if visited[i]:
+            continue
+        dfs(i)
+
+dfs(v)
+print(" ".join(map(str, result)))
+
+
+visited = [False for _ in range(n+1)]
+def bfs(x):
+    tmp = deque()
+    tmp.append(x)
+    visited[x] = True
+    
+    while len(tmp) != 0 :
+        target = tmp.popleft()
+        result2.append(target)
+        
+        for i in arr[target]:
+            if visited[i] :
+                continue
+
+            tmp.append(i)
+            visited[i] = True
+
+bfs(v)
+print(" ".join(map(str, result2)))
+        
