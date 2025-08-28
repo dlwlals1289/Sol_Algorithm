5f86f82ffd96c3d8c173a8a11aae0dd04aa0c9cd
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-08-28 19:04:52 +0900

    [level 3] Title: 네트워크, Time: 0.89 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,17 @@
 def solution(n, computers):
     answer = 0
-    
+    n = len(computers)
     visited = [False] * n
-              
-    def dfs(x):
-        for i in range(n):
-            if i != x and visited[i] != True and computers[x][i] == 1:
-                visited[i] = True
-                dfs(i)
     
+    def dfs(node):
+        visited[node] = True
+        for i in range(n):
+            if computers[node][i] == 1 and not visited[i]:
+                dfs(i)
+                
     for i in range(n):
         if visited[i] == False:
             dfs(i)
             answer += 1
-            
             
     return answer
