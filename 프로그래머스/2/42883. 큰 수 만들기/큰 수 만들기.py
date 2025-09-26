4fd70a1ba9a13a677144b0d23391cd5ae6bdfecd
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-09-26 14:03:37 +0900

    [level 2] Title: 큰 수 만들기, Time: 87.84 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(number, k):
+    answer = ''
+    stack = []
+    
+    for num in number:
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+    
+    while k > 0:
+        stack.pop()
+        k -= 1
+    answer = ''.join(stack)
+    return answer
