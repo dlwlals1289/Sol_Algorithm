6706010ba573dd2796058109373af10c31e5ff20
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-09-26 15:04:55 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,13 +1,15 @@
 def solution(s):
     answer = ''
-    arr = s.split(' ')
-    jadencase = []
+    arr_str = s.split(' ')
+    n = len(arr_str)
     
-    for i in range(len(arr)):
-        if arr[i] == '':
-            jadencase.append(arr[i])
-        else :
-           jadencase.append(arr[i][0].upper()+ arr[i][1:].lower())
-    
-    answer = ' '.join(jadencase)
+    for i, s in enumerate(arr_str):
+        if s != '':
+            first, str_tmp = s[0:1], s[1:]
+            first = first.upper()
+            str_tmp = str_tmp.lower()
+            answer += (first + str_tmp)
+        
+        if i != n-1:
+            answer += ' '
     return answer
