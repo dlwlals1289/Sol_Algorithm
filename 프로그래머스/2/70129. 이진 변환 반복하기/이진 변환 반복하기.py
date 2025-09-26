7a4a2399845ccb47345b0c34711d42ee3aa90b51
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-09-26 15:22:28 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 26.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,10 +1,17 @@
 def solution(s):
-    answer = [0,0]
+    answer = [0, 0]
     
-    while s != "1":
-        answer[1] += s.count('0')
-        s = s.replace('0', '')
-        s = bin(len(s))[2:]
+    while s != '1':
+        arr = list(s)
+        one = 0
+        
+        for i in arr:
+            if i == '0':
+                answer[1] += 1
+            else:
+                one += 1
+        
+        s = bin(one)[2:]
         answer[0] += 1
-    
+        
     return answer
