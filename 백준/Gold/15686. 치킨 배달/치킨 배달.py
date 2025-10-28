64103df77ff5448861be7ec5f599dfd8e25f3aa6
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2025-10-28 13:23:40 +0900

    [Gold V] Title: 치킨 배달, Time: 488 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,30 +1,32 @@
 # 치킨 배달
 import sys
 from itertools import combinations
+import math
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-city = []
-house = []
-chicken = []
-answer = 2000 # (50 + 50)*13 보다 큰 수
-for i in range(n):
-    city.append(list(map(int, input().split())))
+town = []
+houses = []
+chikens = []
+answer = 2*n*n
+
+for _ in range(n):
+    town.append(list(map(int, input().split())))
 
 for i in range(n):
     for j in range(n):
-        if city[i][j] == 1:
-            house.append([i,j])
-        elif city[i][j] == 2:
-            chicken.append([i,j])
+        if town[i][j] == 1:
+           houses.append((i,j))
+        elif town[i][j] == 2:
+            chikens.append((i,j))
 
-for i in combinations(chicken, m):
-    temp = 0
-    for h in house:
-        distance = 100
-        for j in range(m):
-            distance = min(distance, abs(h[0]-i[j][0])+abs(h[1]-i[j][1])) # 집과 치킨집 사이 거리 최솟값 구하기
-        temp += distance
-    answer = min(answer, temp)
-
+for selected in combinations(chikens, m):
+    total = 0
+    for r1, c1 in houses:
+        distance = 2*n
+        
+        for r2, c2 in selected:
+            distance = min(abs(r1-r2)+abs(c1-c2), distance)
+        total += distance
+    answer = min(answer, total)
 print(answer)
