c740ba457aaad52e6149888b7941898b7fc8c12c
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2026-02-01 13:20:29 +0900

    [level 3] Title: 가장 먼 노드, Time: 25.17 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 19 MB, 시간: 42.95 ms
+메모리: 19.8 MB, 시간: 25.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 01일 18:55:23
+2026년 02월 01일 13:20:27
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,29 +1,25 @@
-import math
-from collections import deque
-
+from collections import deque, Counter
 def solution(n, edge):
     answer = 0
-    dq = deque()
-    arr = [[]*n for _ in range(n+1)]
-    distance = [-1] * (n+1)
-    distance[1] = 0
-    dq.append(1)
-    
-    for i in range(len(edge)):
-        a, b = edge[i]
-        arr[a].append(b)
-        arr[b].append(a)
+    graph = [[] for _ in range(n+1)]
+    dist = [-1 for _ in range(n+1)]
+    q = deque([1])
+    dist[1] = 0
+
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
         
     
-    while dq:
-        now = dq.popleft()
+    
+    while q:
+        node = q.popleft()
         
-        for i in arr[now]:
-            if distance[i] == -1:
-                distance[i] = distance[now] + 1
-                dq.append(i)
-            
-    answer = distance.count(max(distance))
-    
-    
+        for i in graph[node]:
+            if dist[i] == -1:
+                q.append(i)
+                dist[i] = dist[node] + 1
+                
+    cnt = Counter(dist)
+    answer = cnt[max(dist)]
     return answer
