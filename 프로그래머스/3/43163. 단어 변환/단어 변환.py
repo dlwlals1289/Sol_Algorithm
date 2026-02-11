2a73c30ae463969866aa3bd5ea3dda60486efbc8
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2026-02-11 17:49:28 +0900

    [level 3] Title: 단어 변환, Time: 0.31 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,29 +1,28 @@
 from collections import deque
+
 def solution(begin, target, words):
     answer = 0
-    n = len(words)
-    m = len(words[0])
+    len_word = len(begin)
+    dist = [0] * (len(words) + 1)
     
     if target not in words:
         return 0
     
-    q = deque([(begin, 0)])
-    
-    visited = set([begin])
+    q = deque([])
+    q.append((begin, len_word))
     
     while q:
-        now, cnt = q.popleft()
+        now, idx = q.popleft()
         
         if now == target:
-            answer = cnt
-        
-        for word in words:
-            if word not in visited:
-                
-                diff = sum(1 for a, b in zip(now, word) if a != b)
-                
-                if diff == 1:
-                    q.append((word, cnt+1))
-                    visited.add(word)
-    
+            return dist[idx]
+        for i, word in enumerate(words):
+            if dist[i] == 0:
+                cnt = 0
+                for j, w in enumerate(word):
+                    if w == now[j]:
+                        cnt += 1
+                if cnt == len_word - 1:
+                    q.append((word,i))
+                    dist[i] = dist[idx] + 1
     return answer
