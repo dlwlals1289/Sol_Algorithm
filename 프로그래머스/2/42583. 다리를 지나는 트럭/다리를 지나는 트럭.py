a1308cb5495e729f7d67071c6a5b931a8497b53a
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2026-02-14 15:44:07 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.03 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,26 +1,23 @@
 from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     n = len(truck_weights)
+    bridge = deque([])
+    trucks = deque(truck_weights)
+    total_weight = 0
+    time = 0
     
-    bridge = deque()
-    sum = 0
-    idx = 0
+    while trucks or bridge:
+        time += 1
+        while bridge and bridge[0][1] <= time:
+            w, t = bridge.popleft()
+            total_weight -= w
+        if trucks and total_weight + trucks[0] <= weight: #다리 위에 트럭이 더 올라갈 수 있는 경우
+            total_weight+=trucks[0]
+            bridge.append((trucks[0], time+bridge_length))
+            trucks.popleft()
+        elif bridge and trucks:
+            time = max(time, bridge[0][1] - 1)
     
-    for i in range(1, bridge_length * n + 2):
-        if idx == n and len(bridge) == 0:
-            break
-            
-        for truck, deadline in bridge:
-            if deadline == i:
-                bridge.popleft()
-                sum -= truck
-                answer = deadline
-                break
-                
-        if idx != n and sum + truck_weights[idx] <= weight:
-            sum += truck_weights[idx]
-            bridge.append((truck_weights[idx], i+bridge_length))
-            idx += 1
-        
-    return answer
+    return time
