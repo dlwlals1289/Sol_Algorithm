eae69980411abe7a63c9acc2004ba678018913a4
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2026-02-19 13:53:28 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 55.09 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,24 +1,20 @@
 from collections import defaultdict
-
 def solution(gems):
-    n = len(gems)
-    answer = [0, n]
-    all_gems = len(set(gems))
-    counter = defaultdict(int)
+    answer = [0, len(gems)]
+    type = set(gems)
+    n = len(type)
+    cnt = defaultdict(int)
     
     left = 0
-    for right in range(n):
-        counter[gems[right]] += 1
-    
-        while len(counter) == all_gems:
-            if right - left < answer[1] - answer[0]:
-                answer = [left+1, right+1]
-            
-            counter[gems[left]] -= 1
-            
-            if counter[gems[left]] == 0:
-                del(counter[gems[left]])
-            left += 1
-                
+    for right, gem in enumerate(gems):
+        cnt[gem] += 1
         
+        while len(cnt) == n and left <= right:
+            if right - left < answer[1] - answer[0]:
+                answer[0], answer[1] = left+1, right+1
+            cnt[gems[left]] -= 1
+            
+            if cnt[gems[left]] == 0:
+                del cnt[gems[left]]
+            left += 1
     return answer
