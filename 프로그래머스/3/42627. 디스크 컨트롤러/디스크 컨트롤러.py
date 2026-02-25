875b808f69db0a53cdb8fb72be5edd36df821d75
JiMin LEE <76999875+dlwlals1289@users.noreply.github.com>
2026-02-25 23:26:17 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.92 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,25 @@
-import heapq
+from heapq import heappush, heappop
+from collections import deque
 def solution(jobs):
     answer = 0
-    jobs.sort()
-    
-    current_time = 0
-    sum_time = 0
     n = len(jobs)
-    
-    heap = []
+    jobs.sort()
+    hq = []
+
+    time = 0
     idx = 0
-    while idx < n or heap:
-        while idx < n and jobs[idx][0] <= current_time:
-            heapq.heappush(heap, [jobs[idx][1], jobs[idx][0]])
-            idx += 1
-        if heap:
-            duration, start = heapq.heappop(heap)
-            sum_time += duration + current_time - start
-            current_time += duration
-        else:
-            current_time = jobs[idx][0]
     
-    return sum_time // n
+    while idx < n or hq:
+        while idx < n and jobs[idx][0] <= time:
+            entered, dur = jobs[idx]
+            heappush(hq, [dur, entered])
+            idx += 1
+        
+        if hq :
+            dur, entered  = heappop(hq)
+            time += dur
+            answer += (time - entered)
+        else:
+            time = jobs[idx][0]
+    
+    return answer // n
